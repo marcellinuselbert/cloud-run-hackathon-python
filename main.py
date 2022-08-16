4f08b833b329b3f1bb0fcbbe3c55324b729fb861
marcellinuselbert11 <marcellinuselbert46@gmail.com>
2022-08-16 15:51:43 +0700

    Add Throw more often

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-moves = ['F', 'T', 'L', 'R']
+moves = ['F','F','T','T','T','T','T','L','R']
+
 
 @app.route("/", methods=['GET'])
 def index():
@@ -30,9 +31,14 @@ def index():
 
 @app.route("/", methods=['POST'])
 def move():
+
     request.get_data()
     logger.info(request.json)
     return moves[random.randrange(len(moves))]
+
+
+
+
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
